2fbcc27ca7040f12e2c5430d75d99b50932f57d9
Saurabh Nayak <saurabh.nayak@techforce.com>
2022-04-22 18:21:21 +0530

    redux using react hooks

--- src/components/CookieContainer.js
+++ src/components/CookieContainer.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import {buyCookie} from '../redux/cookie/cookieActions'
-
+import {useSelector, useDispatch} from 'react-redux'
 function CookieContainer(props) {
+   const count= useSelector(state=>state.cookie.count)
+   const dispatch=useDispatch()
   return (
     <div>
-    <h2>Number of cakes - {props.count} </h2>
-    <button onClick={props.buyCookie}>Buy COokie</button>
+    <h2>Number of cakes - {count} </h2>
+<button onClick={()=>dispatch(buyCookie())}>Buy Cookie</button>
   </div>
   )
 }
-const mapStateToProps = state => {
+
+//Without useSelector and useDispatch hooks
+ /* const mapStateToProps = state => {
     return {
       count: state.cookie.count
     }
@@ -20,9 +24,11 @@ const mapStateToProps = state => {
     return {
       buyCookie: () => dispatch(buyCookie()),
     }
-  }
+  } 
   
   export default connect(
     mapStateToProps,
     mapDispatchToProps
-  )(CookieContainer)
+  )(CookieContainer) */
+  
+  export default CookieContainer
